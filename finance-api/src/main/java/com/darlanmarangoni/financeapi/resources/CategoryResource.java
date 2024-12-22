b85638ecd42fa97c6afbdc923af1bd4e33bddf91
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-12-22 20:25:36 -0300

    feat: list all categories

--- finance-api/src/main/java/com/darlanmarangoni/financeapi/resources/CategoryResource.java
+++ finance-api/src/main/java/com/darlanmarangoni/financeapi/resources/CategoryResource.java
@@ -4,13 +4,10 @@ import com.darlanmarangoni.financeapi.domain.dtos.CategoryDto;
 import com.darlanmarangoni.financeapi.domain.entities.CategoryEntity;
 import com.darlanmarangoni.financeapi.repositories.CategoryRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -27,6 +24,14 @@ public class CategoryResource {
         var category = categoryRepository.save(new CategoryEntity(dto.name()));
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(category.getId()).toUri()).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDto>> getAllCategories() {
+        return ResponseEntity.ok(categoryRepository.findAll()
+                .stream()
+                .map(m -> new CategoryDto(m.getId(), m.getName()))
+                .toList());
     }
 
 }
