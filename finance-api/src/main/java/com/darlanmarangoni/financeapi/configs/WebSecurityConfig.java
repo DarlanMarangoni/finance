fec564bd36e867cdd396cf4d46607dff34907c7f
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-12-31 10:20:41 -0300

    feat: criando usuário de teste

--- finance-api/src/main/java/com/darlanmarangoni/financeapi/configs/WebSecurityConfig.java
+++ finance-api/src/main/java/com/darlanmarangoni/financeapi/configs/WebSecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,7 +22,19 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("darlan")
+                .password("fb8a4a83-0b63-48d2-b46a-0d5891016e80")
+                .roles("USER")
+                .build();
+        System.out.println(user.getPassword());
+        return new InMemoryUserDetailsManager(user);
     }
 }
