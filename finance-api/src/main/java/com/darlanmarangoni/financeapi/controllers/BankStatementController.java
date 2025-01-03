a64ef15a597fed2b1a71d887b9d0bb2d79f365a8
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-01-03 11:04:17 -0300

    feat: form v1

--- finance-api/src/main/java/com/darlanmarangoni/financeapi/controllers/BankStatementController.java
+++ finance-api/src/main/java/com/darlanmarangoni/financeapi/controllers/BankStatementController.java
@@ -1,15 +1,29 @@
 package com.darlanmarangoni.financeapi.controllers;
 
+import com.darlanmarangoni.financeapi.domain.BankStatementType;
+import com.darlanmarangoni.financeapi.domain.entities.CategoryEntity;
+import com.darlanmarangoni.financeapi.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class BankStatementController {
 
+    private final CategoryRepository categoryRepository;
+
+    public BankStatementController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
     @GetMapping("/bankStatement")
     public String bankStatement(Model model) {
-        model.addAttribute("message", "Hello World Extrato");
+        final List<CategoryEntity> categories = categoryRepository.findAll();
+        model.addAttribute("message", "Lançamentos de receitas e despesas");
+        model.addAttribute("bankStatementTypes", BankStatementType.values());
+        model.addAttribute("categories", categories);
         return "bankStatement";
     }
 }
