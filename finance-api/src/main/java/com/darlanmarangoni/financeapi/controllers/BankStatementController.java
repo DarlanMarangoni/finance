03d7060434048236e98b139c4387ce574a101104
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-01-03 22:19:17 -0300

    feat: form v2

--- finance-api/src/main/java/com/darlanmarangoni/financeapi/controllers/BankStatementController.java
+++ finance-api/src/main/java/com/darlanmarangoni/financeapi/controllers/BankStatementController.java
@@ -1,11 +1,14 @@
 package com.darlanmarangoni.financeapi.controllers;
 
 import com.darlanmarangoni.financeapi.domain.BankStatementType;
+import com.darlanmarangoni.financeapi.domain.dtos.BankStatementDto;
 import com.darlanmarangoni.financeapi.domain.entities.CategoryEntity;
 import com.darlanmarangoni.financeapi.repositories.CategoryRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,6 +27,13 @@ public class BankStatementController {
         model.addAttribute("message", "Lançamentos de receitas e despesas");
         model.addAttribute("bankStatementTypes", BankStatementType.values());
         model.addAttribute("categories", categories);
+        model.addAttribute("bankStatementDto", new BankStatementDto());
+        return "bankStatement";
+    }
+
+    @PostMapping("/bankStatement")
+    public String create(@ModelAttribute BankStatementDto dto, Model model) {
+        System.out.println(dto);
         return "bankStatement";
     }
 }
--- finance-api/src/main/java/com/darlanmarangoni/financeapi/domain/dtos/BankStatementDto.java
+++ finance-api/src/main/java/com/darlanmarangoni/financeapi/domain/dtos/BankStatementDto.java
@@ -4,13 +4,91 @@ import com.darlanmarangoni.financeapi.domain.BankStatementType;
 
 import java.time.LocalDate;
 
-public record BankStatementDto(
-        String categorie,
-        String name,
-        String value,
-        LocalDate releaseDate,
-        LocalDate dueDate,
-        LocalDate paymentDate,
-        String description,
-        BankStatementType bankStatementType
-) {}
+public class BankStatementDto {
+    private String categorie;
+    private String name;
+    private String value;
+    private LocalDate releaseDate;
+    private LocalDate dueDate;
+    private LocalDate paymentDate;
+    private String description;
+    private BankStatementType bankStatementType;
+
+    public String getCategorie() {
+        return categorie;
+    }
+
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BankStatementType getBankStatementType() {
+        return bankStatementType;
+    }
+
+    public void setBankStatementType(BankStatementType bankStatementType) {
+        this.bankStatementType = bankStatementType;
+    }
+
+    @Override
+    public String toString() {
+        return "BankStatementDto{" +
+                "categorie='" + categorie + '\'' +
+                ", name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", dueDate=" + dueDate +
+                ", paymentDate=" + paymentDate +
+                ", description='" + description + '\'' +
+                ", bankStatementType=" + bankStatementType +
+                '}';
+    }
+}
