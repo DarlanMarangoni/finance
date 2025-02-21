6c464f04fb9835d2f4ad1e5ab433a7d09a7ec673
Darlan Marangoni <marangoni.darlan1@gmail.com>
2025-02-20 21:08:37 -0300

    feat: add menubar

--- finance-app/src/app/app.component.ts
+++ finance-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Button } from 'primeng/button';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button],
+  imports: [RouterOutlet, Button, HeaderComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
